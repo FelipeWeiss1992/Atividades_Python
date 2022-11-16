d9a73d2fc28f2c9ef191bff69b36d67573a755bf
FelipeWeiss1992 <felipeweiss92@gmail.com>
2022-11-16 20:01:42 -0300

    update

--- Orientacao a Objeto/OPP_Herança_GetterSetter/pessoaFisica.py
+++ Orientacao a Objeto/OPP_Herança_GetterSetter/pessoaFisica.py
@@ -13,36 +13,37 @@ class PessoaFisica(Conta):
         print('Passando pelo contrutor pessoa fisica')
         
     @property
-    def titular_conta(self):
-        return self.__titular
-
-    @titular_conta.setter
-    def titular_conta(self, titular):
-        self.__titular = titular
-    
-    @property
-    def numero_cpf(self):
-        return self.__cpf
-
-    @numero_cpf.setter
-    def numero_cpf(self, cpf):
-        self.__cpf = cpf
-    
-    @property
-    def saldo_inicial_conta(self):
-        return self.__saldo_inicial
-
-    @saldo_inicial_conta.setter
-    def saldo_inicial_conta(self, saldo):
-        self.__saldo_inicial = saldo  
-
-    @property
     def segundo_titular(self):
         return self.__segundo_titular
     
     @segundo_titular.setter
     def segundo_titular(self, segundo_titular):
         self.__segundo_titular = segundo_titular
+
+    @property
+    def titular(self):
+        return self.__titular
+
+    @titular.setter
+    def titular(self, titular):
+        self.__titular = titular.title()
+    
+    @property
+    def cpf(self):
+        return self.__cpf
+
+    @cpf.setter
+    def cpf(self, cpf):
+        self.__cpf = cpf
+    
+    @property
+    def saldo_inicial(self):
+        return self.__saldo_inicial
+
+    @saldo_inicial.setter
+    def saldo_inicial(self, saldo):
+        self.__saldo_inicial = saldo  
+
 
     def __str__(self):
         return f'{super().__str__()} - Titular: {self.titular_conta} - CPF: {self.numero_cpf} - Saldo Inicial: {self.saldo_inicial_conta} - Segundo Titular: {self.segundo_titular}'
