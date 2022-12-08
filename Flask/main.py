e3fa5550117c2018f9801ff4ba8aac6320d6b20b
FelipeWeiss1992 <felipeweiss92@gmail.com>
2022-12-08 19:39:07 -0300

    update flask

--- Flask/main.py
+++ Flask/main.py
@@ -1,23 +1,37 @@
-from flask import Flask,render_template
+from flask import Flask,render_template, request, redirect
 from pessoa import Pessoa
 
+
+pessoa1 = Pessoa('Felipe','30','1.80')
+pessoa2 = Pessoa('Haiko','15','1.50')
+pessoa3 = Pessoa('Jean','42','1.95')
+
+lista = [pessoa1, pessoa2, pessoa3]
 #Variavel de referencia FLask
 app = Flask(__name__)
 
 #Criando Rota   
 @app.route("/")
 def inicio():
-    pessoa1 = Pessoa('Felipe','30','1.80')
-    pessoa2 = Pessoa('Haiko','15','1.50')
-    pessoa3 = Pessoa('Jean','42','1.95')
 
-    lista = [pessoa1, pessoa2, pessoa3]
+    return render_template("index.html", titulo = "Lista Pessoas", pessoas = lista)
 
-    return render_template("index.html", titulo = "Tabela", pessoas = lista)
-
-@app.route("/novo")
+@app.route("/cadastrar")
 def novo():
-    return "Titulo Rota Nova"
+    return render_template('novo.html', titulo = 'Cadastro Pessoa')
+
+@app.route('/criar', methods=['POST'])
+def criar():
+    nome = request.form['nome']
+    idade = request.form['idade']
+    altura = request.form['altura']
+
+    pessoa = Pessoa(nome, idade, altura)
+    lista.append(pessoa)
+
+    return redirect ('/')
 
 #Iniciando
-app.run()
+
+app.run(debug=True)
+
