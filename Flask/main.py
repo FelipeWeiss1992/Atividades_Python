bd9a465ac7409717a6300fd25f6d91f55452c8e2
FelipeWeiss1992 <felipeweiss92@gmail.com>
2022-12-08 21:48:58 -0300

    update flask

--- Flask/main.py
+++ Flask/main.py
@@ -1,15 +1,11 @@
 from flask import Flask, render_template, request, redirect
 from pessoa import Pessoa
 
-pessoa1 = Pessoa('haiko', '15', '1:50')
-pessoa2 = Pessoa('Jean', '42', '1:84')
-pessoa3 = Pessoa('Gisele', '16', '1:56')
 
-lista = [pessoa1, pessoa2, pessoa3]
+lista = []
 
 app = Flask(__name__)
 
-#
 @app.route('/')
 def inicio():
     return render_template('index.html', titulo = 'Lista Pessoas', pessoas = lista)
@@ -18,7 +14,7 @@ def inicio():
 def novo():
     return render_template('novo.html', titulo = 'Cadastro Pessoa')
 
-@app.route('/criar', methods=['POST',])
+@app.route('/criar', methods=['POST'])
 def criar():
     nome = request.form['nome']
     idade = request.form['idade']
