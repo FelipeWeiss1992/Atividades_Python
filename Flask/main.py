a4a9fbd593ef5cc5a3b2f5e47ae7465a88efecb2
FelipeWeiss1992 <felipeweiss92@gmail.com>
2022-12-08 21:01:34 -0300

    update flask

--- Flask/main.py
+++ Flask/main.py
@@ -1,37 +1,34 @@
-from flask import Flask,render_template, request, redirect
+from flask import Flask, render_template, request, redirect
 from pessoa import Pessoa
 
-
-pessoa1 = Pessoa('Felipe','30','1.80')
-pessoa2 = Pessoa('Haiko','15','1.50')
-pessoa3 = Pessoa('Jean','42','1.95')
+pessoa1 = Pessoa('haiko', '15', '1:50')
+pessoa2 = Pessoa('Jean', '42', '1:84')
+pessoa3 = Pessoa('Gisele', '16', '1:56')
 
 lista = [pessoa1, pessoa2, pessoa3]
-#Variavel de referencia FLask
+
 app = Flask(__name__)
 
-#Criando Rota   
-@app.route("/")
+#
+@app.route('/')
 def inicio():
+    return render_template('index.html', titulo = 'Lista Pessoas', pessoas = lista)
 
-    return render_template("index.html", titulo = "Lista Pessoas", pessoas = lista)
-
-@app.route("/cadastrar")
+@app.route('/novo')
 def novo():
     return render_template('novo.html', titulo = 'Cadastro Pessoa')
 
-@app.route('/criar', methods=['POST'])
+@app.route('/criar', methods=['POST',])
 def criar():
     nome = request.form['nome']
     idade = request.form['idade']
     altura = request.form['altura']
 
-    pessoa = Pessoa(nome, idade, altura)
-    lista.append(pessoa)
+    pessoas = Pessoa(nome, idade, altura)
 
-    return redirect ('/')
+    lista.append(pessoas)
 
-#Iniciando
+    return redirect('/')
+
 
 app.run(debug=True)
-
