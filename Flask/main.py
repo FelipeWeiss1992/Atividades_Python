9641fa332d092835c8b84ba7a2e46eb762333f76
FelipeWeiss1992 <felipeweiss92@gmail.com>
2022-12-12 21:20:00 -0300

    update flask

--- Flask/main.py
+++ Flask/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session, flash
 from pessoa import Pessoa
 
 pessoa1 = Pessoa('Felipe', '30', 1.81)
@@ -9,16 +9,24 @@ pessoa3 = Pessoa('Jean', '40', 1.85)
 lista = [pessoa1,pessoa2,pessoa3]
 
 app = Flask(__name__)
+app.secret_key = 'moredevs'
 
 @app.route('/')
+def login():
+    return render_template('login.html', titulo = 'Login Usuario')
+
+@app.route('/listar')
 def inicio():
     return render_template('index.html', titulo = 'Lista Pessoas', pessoas = lista)
 
 @app.route('/novo')
 def novo():
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        return redirect('/')
+
     return render_template('novo.html', titulo = 'Cadastro Pessoa')
 
-@app.route('/criar', methods=['POST'])
+@app.route('/criar', methods=['post'])
 def criar():
     nome = request.form['nome']
     idade = request.form['idade']
@@ -28,7 +36,25 @@ def criar():
 
     lista.append(pessoa)
 
-    return redirect('/')
+    return redirect('/listar')
 
+@app.route('/autenticar', methods = ['post'])
+def autenticar():
+    if 'moredevs' == request.form['senha']:
+
+        session['usuario_logado'] = request.form['usuario']
+
+        flash(session['usuario_logado'] + ' ' + 'Logado')
+        return redirect('/listar')
+
+    else:
+        flash('Senha incorreta')
+        return redirect('/')
+
+@app.route('/logout')
+def logout():
+    session['usuario_logado'] == None
+    flash('Você foi desconectado')
+    return redirect('/')
 
 app.run(debug=True)
